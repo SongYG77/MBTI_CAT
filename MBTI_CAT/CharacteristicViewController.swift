671f7f705af5b1396ae602fc5a009b515e7117c8
SongYunGeun <rjawoakstp@naver.com>
2022-01-24 17:43:08 +0900

    고양이 별 특징 화면 구성중, 디테일로 넘어갈 때 스크롤 뷰에 대한 레이아웃 문제(해결필요)

--- MBTI_CAT/CharacteristicViewController.swift
+++ MBTI_CAT/CharacteristicViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class CharacteristicViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
     }
     
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - 버튼들
+    @IBAction func onBtnBelinese(_ sender: UIButton) {
+        
+        let NextVC = self.storyboard?.instantiateViewController(identifier: "DetailContentsVC") as! DetailContentsViewController
+        
+        NextVC.CatKinds = "Belinese"
+        
+        self.navigationController?.pushViewController(NextVC, animated: true)
+        
     }
-    */
+    
 
 }
---
+++ MBTI_CAT/DetailContentsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailContentsViewController.swift
+//  MBTI_CAT
+//
+//  Created by 송윤근 on 2022/01/24.
+//
+
+import UIKit
+
+class DetailContentsViewController: UIViewController {
+    
+    
+    var CatKinds : String = ""
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(CatKinds)
+    }
+    
+
+    
+
+}
